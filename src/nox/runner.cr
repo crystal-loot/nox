77c5042e700e8e6d03b9ab52da861ce76da0614a
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-12-22 23:02:49 -0600

    Add little starting message

--- src/nox/runner.cr
+++ src/nox/runner.cr
@@ -2,11 +2,12 @@ class Nox::Runner
   private getter done = Channel(Nil).new
   private getter processes : Array(Nox::Process)
 
-  def initialize(procfile : Nox::Procfile)
-    @processes = procfile.entries.map { |entry| Nox::Process.new(entry, dir: Dir.current, output: STDOUT) }
+  def initialize(procfile : Nox::Procfile, @output = STDOUT)
+    @processes = procfile.entries.map { |entry| Nox::Process.new(entry, dir: Dir.current, output: @output) }
   end
 
   def run
+    @output.puts "Nox starting processes..."
     processes.each do |process|
       spawn do
         process.run
