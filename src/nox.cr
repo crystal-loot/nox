71c22394bbb82821d65736ee42207083102bfa26
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-12-30 21:36:15 -0600

    v0.2.0

--- src/nox.cr
+++ src/nox.cr
@@ -5,7 +5,7 @@ require "./nox/process"
 require "./nox/runner"
 
 module Nox
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 
   def self.run(file : String)
     procfile = Nox::Procfile.parse_file(file)
