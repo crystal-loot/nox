fbe9f9d2eef89b5313a712d86dcf123b0d2c9835
Matthew McGarvey <mmcgarvey@rizemoney.com>
2022-09-14 10:06:57 -0500

    Check if process exists before killing/interrupting

--- src/nox/process.cr
+++ src/nox/process.cr
@@ -16,20 +16,25 @@ class Nox::Process
     print_bold "Starting with pid of #{process.pid}"
     process.wait
     print_bold "Done"
-    @process = nil
   end
 
   def interrupt
-    if process = @process
+    with_process do |process|
       print_bold "Attempting to interrupt..."
       process.signal(Signal::INT)
     end
   end
 
   def kill
-    if process = @process
+    with_process do |process|
       print_bold "Attempting to kill..."
       process.signal(Signal::KILL)
+    end
+  end
+
+  def with_process
+    if (process = @process) && process.exists?
+      yield process
     end
   end
 
