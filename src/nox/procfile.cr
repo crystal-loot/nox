a6f97fa5c70aa63ea4a39393d286611b58ab1669
Matthew McGarvey <mmcgarvey@rizemoney.com>
2022-10-04 08:39:24 -0500

    Handle blank lines and comments in procfiles

--- src/nox/procfile.cr
+++ src/nox/procfile.cr
@@ -8,7 +8,9 @@ struct Nox::Procfile
   def self.parse(content : String) : self
     proc_file = new
     content.each_line do |line|
-      match_data = ENTRY_REGEX.match(line).not_nil!
+      match_data = ENTRY_REGEX.match(line)
+      next if match_data.nil?
+
       proc_file.entries << Nox::Procfile::Entry.new(match_data[1], match_data[2])
     end
     proc_file
